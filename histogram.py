006f2cd93e103171fea79771af6fe58be07656f2
smhash78 <smhash11@gmail.com>
2024-01-19 23:38:39 +0330

    Added modify_bits in histogram.py

--- histogram.py
+++ histogram.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 from matplotlib import use
@@ -9,7 +10,7 @@ use('TkAgg')
 def plot_histogram(img, name="image"):
     # Calculate histogram
     bit_depth = img.dtype.itemsize * 8
-    img_gray_level_res = 2**bit_depth
+    img_gray_level_res = 2 ** bit_depth
     hist = cv2.calcHist([img], [0], None, [img_gray_level_res], [0, img_gray_level_res])
 
     # Plot histogram
@@ -20,7 +21,33 @@ def plot_histogram(img, name="image"):
     plt.show()
 
 
+def modify_bits(img, bit_changes):
+    """
+    The image bits will be changed based on the pattern in bit_changes.
+    For each bit of all pixels in the image, if the corresponding value
+    in the bit_changes list is -1, the bit will be left unchanged, if
+    it is 0, the bit will be set to 0 and if it is 1, the bit will be
+    set to 1.
+    """
+    bit_depth = len(bit_changes)
+
+    modified_img = np.copy(img)
+    for i, change in enumerate(bit_changes):
+        if change == 0:
+            modified_img = modified_img & ~(1 << (bit_depth - i - 1))
+        elif change == 1:
+            modified_img = modified_img | (1 << (bit_depth - i - 1))
+
+    modified_img = modified_img.astype(np.uint8)
+
+    return modified_img
+
+
 if __name__ == '__main__':
     image_path = './data/input/lena.png'
+
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    plot_histogram(image)
+    plot_histogram(image, "original Lena")
+
+    modified_image = modify_bits(image, [-1, -1, -1, -1, 0, 0, 0, 0])
+    plot_histogram(modified_image, "modified Lena")
